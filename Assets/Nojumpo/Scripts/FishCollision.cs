a33128ba33fa5fbf052c16ff79cfeadaee17d4b5
Berke Konargöçer <berke.konargocer@gmail.com>
2023-06-02 23:04:08 +0300

    Fish Flash Feedback On Collision With Pipe

--- Assets/Nojumpo/Scripts/FishCollision.cs
+++ Assets/Nojumpo/Scripts/FishCollision.cs
@@ -1,19 +1,30 @@
+using DG.Tweening;
 using Nojumpo.Enums;
 using Nojumpo.Scripts;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace Nojumpo
 {
     public class FishCollision : MonoBehaviour
     {
         // -------------------------------- FIELDS ---------------------------------
-
+        SpriteRenderer _fishSpriteRenderer;
+        
+        [SerializeField] Color flashOnHitColor;
+        
+        
         // ------------------------- UNITY BUILT-IN METHODS ------------------------
+        void Awake() {
+            SetComponents();
+        }
+        
         void OnTriggerEnter2D(Collider2D other) {
             if (other.gameObject.CompareTag("Pipe"))
             {
                 if (GameManager.Instance.CurrentGameState != GameState.DEAD)
                 {
+                    FishCollisionFlash();
                     GameManager.Instance.RaiseOnDieEvent();
                 }
             }
@@ -21,7 +32,14 @@ namespace Nojumpo
 
 
         // ------------------------- CUSTOM PRIVATE METHODS ------------------------
-
+        void SetComponents() {
+            _fishSpriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        
+        void FishCollisionFlash() {
+            _fishSpriteRenderer.color = flashOnHitColor;
+            _fishSpriteRenderer.DOColor(Color.white, 0.75f);
+        }
 
 
         // ------------------------- CUSTOM PUBLIC METHODS -------------------------
